28516f0c3a01487bba9798f572a0b9af4202d5a5
Hendrik M Halkow <hendrik@halkow.com>
2019-04-24 11:57:06 +0200

    Make script run on Fedora

--- build.sh
+++ build.sh
@@ -20,31 +20,68 @@ trap cleanup INT EXIT
 
 cleanup() {
   test -n "${ctr}" && buildah rm "${ctr}" || true
+  test -f "${yum_config_file}" && rm "${yum_config_file}" || true
+  test -f "${rpm_public_key_file}" && rm "${rpm_public_key_file}" || true
 }
 
 image="centos"
 
+# Create container
 ctr="$(buildah from scratch)"
+
+# Mount container
 mnt="$(buildah mount ${ctr})"
+
+# Initialize RPM database
+mkdir -p "${mnt}/var/lib/rpm"
+rpm --root "${mnt}" --initdb
+
+# Download CentOS GnuPG key via HTTPS
+rpm_public_key_file="$(mktemp)"
+curl --location https://www.centos.org/keys/RPM-GPG-KEY-CentOS-7 \
+  > "${rpm_public_key_file}"
+
+# Import key so that package signature can be checked
+rpm --root "${mnt}" --import "${rpm_public_key_file}"
+
+# Write Yum config file that is used during initial installation
+yum_config_file="$(mktemp)"
+cat <<EOD > "${yum_config_file}"
+[centos-buildah-base]
+name=CentOS-7-Base
+baseurl=http://mirror.centos.org/centos/7/os/x86_64
+gpgcheck=1
+EOD
+
+# Options that are used with every `yum` command
 yum_opts=(
+  "--config=${yum_config_file}"
+  "--disablerepo=*"
+  "--enablerepo=centos-buildah-base"
+  "--disableplugin=*"
   "--installroot=${mnt}"
   "--assumeyes"
   "--setopt=install_weak_deps=false"
   "--releasever=7"
   "--setopt=tsflags=nodocs"
-  "--setopt=override_install_langs=en_US.utf8"
 )
 
+# Install CentOS
 yum ${yum_opts[@]} install centos-release.x86_64
 yum ${yum_opts[@]} clean all
 
-bom_sha256="$(rpm \
+# Get a bill of materials
+bill_of_materials="$(rpm \
   --query \
   --all \
-  --queryformat \
-    '\{ "type": "rpm", "name": "%{NAME}", "version": "%{VERSION}", "release": "%{RELEASE}", "arch": "%{ARCH}"\}\n' \
+  --queryformat "%{NAME} %{VERSION} %{RELEASE} %{ARCH}" \
   --dbpath="${mnt}"/var/lib/rpm \
-  | sort | jq -cs '.' | sha256sum | awk '{ print $1; }')"
+  | sort )"
+
+# Get bill of materials hash – the content
+# of this script is included in hash, too.
+bill_of_materials_hash="$( (cat "${0}"; echo "${bill_of_materials}") \
+  | sha256sum | awk '{ print $1; }' )"
 
 version="$( perl -0777 -ne 'print "$&\n" if /\d+(\.\d+)*/' \
   "${mnt}/etc/centos-release" )"
@@ -73,8 +110,6 @@ echo 'root:*:0:0:99999:7:::' > ./etc/shadow
 
 popd
 
-buildah run "${ctr}" -- localedef --force --inputfile=en_US --charmap=UTF-8 en_US.UTF-8
-
 oci_prefix="org.opencontainers.image"
 buildah config \
   --label "${oci_prefix}.authors=SDA SE Engineers <cloud@sda-se.com>" \
@@ -86,8 +121,11 @@ buildah config \
   --label "${oci_prefix}.licenses=AGPL-3.0" \
   --label "${oci_prefix}.title=CentOS" \
   --label "${oci_prefix}.description=CentOS base image" \
-  --label "io.sda-se.image.bom.sha256=$( echo "${bom_sha256}" )" \
+  --label "io.sda-se.image.bill-of-materials-hash=$( \
+    echo "${bill_of_materials_hash}" )" \
   --env "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
+  --env "LANG=en_US.UTF-8" \
+  --env "LC_ALL=en_US.UTF-8" \
   --cmd "/bin/sh" \
   "${ctr}"
 
