6272748a32e03dd5145545d2dc9b603dc46fa911
Christian Groschupp <christian@groschupp.org>
2020-05-25 07:44:43 +0200

    switch to centos8 (#8)
    * switch to centos8
    
    * add nocontexts to tsflags
    
    * install coreutils
    
    * remove github workflow
    
    * add centos:7 image

--- build.sh
+++ build.sh
@@ -26,7 +26,138 @@ cleanup() {
   test -f "${rpm_public_key_file}" && rm "${rpm_public_key_file}" || true
 }
 
-image="centos"
+image="centos:8"
+
+# Create container
+ctr="$( buildah from scratch )"
+
+# Mount container
+mnt="$( buildah mount "${ctr}" )"
+
+# Initialize RPM database.
+mkdir -p "${mnt}"/var/lib/rpm
+rpm --root "${mnt}" --initdb
+
+# Download CentOS GnuPG key via HTTPS
+rpm_public_key_file="$( mktemp )"
+curl --location https://www.centos.org/keys/RPM-GPG-KEY-CentOS-Official \
+  > "${rpm_public_key_file}"
+
+# Import key so that package signature can be checked
+rpm --root "${mnt}" --import "${rpm_public_key_file}"
+
+# Write Yum config file that is used during initial installation
+yum_config_file="$( mktemp )"
+cat <<EOD > "${yum_config_file}"
+[centos-buildah-baseos]
+name=CentOS-8 - BaseOS
+baseurl=http://mirror.centos.org/centos/8/BaseOS/x86_64/os/
+gpgcheck=1
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+
+[centos-buildah-appstream]
+name=CentOS-8 - AppStream
+baseurl=http://mirror.centos.org/centos/8/AppStream/x86_64/os/
+gpgcheck=1
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+
+[centos-buildah-extras]
+name=CentOS-8 - Extras
+baseurl=http://mirror.centos.org/centos/8/extras/x86_64/os/
+gpgcheck=1
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
+EOD
+
+# Options that are used with every `yum` command
+dnf_opts=(
+  "--config=${yum_config_file}"
+  "--disablerepo=*"
+  "--enablerepo=centos-buildah-baseos,centos-buildah-appstream,centos-buildah-extras"
+  "--disableplugin=*"
+  "--installroot=${mnt}"
+  "--assumeyes"
+  "--setopt=install_weak_deps=false"
+  "--releasever=8"
+  "--setopt=tsflags=nocontexts,nodocs"
+)
+
+# Install CentOS
+dnf ${dnf_opts[@]} install bash coreutils rpm glibc-minimal-langpack glibc-langpack-de glibc-langpack-en
+dnf ${dnf_opts[@]} clean all
+
+# Get a bill of materials
+bill_of_materials="$(rpm \
+  --query \
+  --all \
+  --queryformat "%{NAME} %{VERSION} %{RELEASE} %{ARCH}" \
+  --dbpath="${mnt}"/var/lib/rpm \
+  | sort )"
+
+# Get bill of materials hash – the content
+# of this script is included in hash, too.
+bill_of_materials_hash="$( (cat "${0}"; echo "${bill_of_materials}") \
+  | sha256sum | awk '{ print $1; }' )"
+
+version="$( perl -0777 -ne 'print "$&\n" if /\d+(\.\d+)*/' \
+  "${mnt}/etc/centos-release" )"
+
+pushd "${mnt}"
+
+mkdir -p run/lock
+
+# System directories
+rm -rf boot dev proc sys
+
+# Unnecessary stuff
+rm -rf home media mnt opt srv
+
+# Stuff that prevents reproduceable build
+rm -rf \
+  etc/machine-id \
+  lib/.build-id \
+  etc/pki/ca-trust/extracted/java/cacerts \
+  var/{cache,log}/* \
+  tmp/*
+
+popd
+
+oci_prefix="org.opencontainers.image"
+buildah config \
+  --label "${oci_prefix}.authors=SDA SE Engineers <engineers@sda-se.io>" \
+  --label "${oci_prefix}.url=https://quay.io/sdase/centos" \
+  --label "${oci_prefix}.source=https://github.com/SDA-SE/centos" \
+  --label "${oci_prefix}.version=${version}" \
+  --label "${oci_prefix}.revision=$( git rev-parse HEAD )" \
+  --label "${oci_prefix}.vendor=SDA SE Open Industry Solutions" \
+  --label "${oci_prefix}.licenses=AGPL-3.0" \
+  --label "${oci_prefix}.title=CentOS" \
+  --label "${oci_prefix}.description=CentOS base image" \
+  --label "io.sda-se.image.bill-of-materials-hash=$( \
+    echo "${bill_of_materials_hash}" )" \
+  --env "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
+  --env "LANG=en_US.UTF-8" \
+  --env "LC_ALL=en_US.UTF-8" \
+  --cmd "/bin/sh" \
+  "${ctr}"
+
+# Create image
+buildah commit --quiet --rm --squash "${ctr}" "${image}" && ctr=
+
+if [ -n "${BUILD_EXPORT_OCI_ARCHIVES}" ]
+then
+  mkdir --parent "${build_dir}"
+  buildah push --quiet \
+    "${image}" \
+    "oci-archive:${build_dir}/${image//:/-}.tar"
+  buildah rmi "${image}"
+fi
+
+cleanup
+
+image="centos:7"
 
 # Create container
 ctr="$( buildah from scratch )"
@@ -53,7 +184,6 @@ cat <<EOD > "${yum_config_file}"
 name=CentOS-7-Base
 baseurl=http://mirror.centos.org/centos/7/os/x86_64
 gpgcheck=1
-
 [centos-buildah-updates]
 name=CentOS-7 - Updates
 baseurl=http://mirror.centos.org/centos/7/updates/x86_64/
@@ -70,7 +200,7 @@ yum_opts=(
   "--assumeyes"
   "--setopt=install_weak_deps=false"
   "--releasever=7"
-  "--setopt=tsflags=nodocs"
+  "--setopt=tsflags=nocontexts,nodocs"
 )
 
 # Install CentOS
